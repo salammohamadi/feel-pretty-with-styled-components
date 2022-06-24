55d5416cfe6fe0f1de2335731a160858c2ff9444
Salam Mohamadi <mohamadi.salam@gmail.com>
2022-06-24 15:07:53 +0430

    find flws in global CSS and inline style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return <h2>styled components</h2>;
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1 className='title'>styled components</h1>
+      <button className='btn'>click me</button>
+      <div className='title'>
+        <h2>hello world</h2>
+      </div>
+    </div>
+  );
 }
-
 export default App;
