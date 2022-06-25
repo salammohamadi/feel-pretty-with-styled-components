a81e9269e50f82fecc679f369becfcd55cd7ad52
Salam Mohamadi <mohamadi.salam@gmail.com>
2022-06-25 16:12:48 +0430

    set up a GlobalStyle component and add it to root component

---
+++ src/global-styles.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+:root {
+  --primary: #645cff;
+  --white: #fff;
+}
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: #f2f4f8;
+}
+
+`;
+
+export default GlobalStyles;
